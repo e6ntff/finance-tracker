be24c8cc8478095fc08c52a59086c87955ce5899
e6ntff <pashatar2@gmail.com>
2023-11-08 13:06:46 +0300

    added select label

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,14 +3,21 @@ import React from 'react'
 import styles from './Select.module.css'
 
 const Select = (props) => {
-	return (
-		<select name="yearSelect" className={styles.select} onChange={props.handleYearChanging}>
-			<option value="2023">2023</option>
-			<option value="2022">2022</option>
-			<option value="2021">2021</option>
-			<option value="2020">2020</option>
-		</select>
-	)
+  return (
+    <label className={styles.label}>
+      <select
+        name="yearSelect"
+        className={styles.select}
+        onChange={props.handleYearChanging}
+      >
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+      </select>
+			<span className={styles.text}>Filter by year</span>
+    </label>
+  )
 }
 
 export default Select

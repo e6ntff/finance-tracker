30e1a9aba3b27953fb30779beb51275dce8695d6
e6ntff <pashatar2@gmail.com>
2023-11-09 14:37:43 +0300

    fixed bar transition

--- src/components/Diagram/DiagramBar/DiagramBar.js
+++ src/components/Diagram/DiagramBar/DiagramBar.js
@@ -3,11 +3,13 @@ import React from 'react'
 import styles from './DiagramBar.module.css'
 
 const DiagramBar = (props) => {
-  const value = (props.value / props.maxValue) * 100 + '%'
+  const value = props.maxValue > 0 ? (props.value / props.maxValue) * 100 : 0
 
   return (
     <li className={styles.bar}>
-      <div className={styles.column} style={{ '--value': value }}></div>
+      <div className={styles.column}>
+        <div className={styles.value} style={{ '--value': value > 100 ? '100%' : `${value}%` }}></div>
+      </div>
       <span className={styles.month}>{props.month}</span>
     </li>
   )

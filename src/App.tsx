44a843018919514182d43ec5d5cf6d80bb7e79ba
e6ntff <pashatar2@gmail.com>
2024-03-11 15:58:59 +0300

    added main preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import AppHeader from './components/AppHeader';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import firebaseApp from './utils/firebase';
 import AppRoutes from './components/AppRoutes';
 import { observer } from 'mobx-react-lite';
-import { ConfigProvider, Layout, theme } from 'antd';
+import { ConfigProvider, Flex, Layout, Spin, theme } from 'antd';
 import { userStore } from 'utils/userStore';
 import getCurrencyRates from 'utils/getCurrencyRates';
 import { Content, Header } from 'antd/es/layout/layout';
@@ -17,6 +17,16 @@ const App: React.FC = observer(() => {
 	const { logged, setTheme } = userStore;
 	const { themeAlgorithm } = userStore;
 	const { setCurrencyRates, setCurrency, setUser } = userStore;
+	const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (logged) setIsLoaded(true);
+		const loadingTimer = setTimeout(() => {
+			setIsLoaded(true);
+		}, 500);
+
+		return () => clearTimeout(loadingTimer);
+	}, [logged]);
 
 	useEffect(() => {
 		getCurrencyRates().then(setCurrencyRates);
@@ -48,46 +58,66 @@ const App: React.FC = observer(() => {
 							minBlockSize: '100%',
 						}}
 					>
-						{logged && (
-							<Header
-								style={{
-									inlineSize: 'min(100%, 960px)',
-									margin: 'auto',
-									position: 'sticky',
-									inset: 0,
-									zIndex: 1,
-									borderEndEndRadius: borderRadiusLG,
-									borderEndStartRadius: borderRadiusLG,
-								}}
-							>
-								<AppHeader />
-							</Header>
-						)}
-						<Layout
-							style={{
-								blockSize: '100%',
-								inlineSize: 'min(100%, 960px)',
-								margin: 'auto',
-							}}
-						>
-							<Layout
-								style={{
-									padding: paddingLG,
-								}}
-							>
-								<Content
+						{isLoaded ? (
+							<>
+								{logged && (
+									<Header
+										style={{
+											inlineSize: 'min(100%, 960px)',
+											margin: 'auto',
+											position: 'sticky',
+											inset: 0,
+											zIndex: 1,
+											borderEndEndRadius: borderRadiusLG,
+											borderEndStartRadius: borderRadiusLG,
+										}}
+									>
+										<AppHeader />
+									</Header>
+								)}
+								<Layout
 									style={{
-										padding: paddingLG,
+										blockSize: '100%',
+										inlineSize: 'min(100%, 960px)',
 										margin: 'auto',
-										inlineSize: `${logged ? '100%' : 'max-content'}`,
-										blockSize: 'min-content',
-										borderRadius: '0.5em',
 									}}
 								>
-									<AppRoutes />
-								</Content>
+									<Layout
+										style={{
+											padding: paddingLG,
+										}}
+									>
+										<Content
+											style={{
+												padding: paddingLG,
+												margin: 'auto',
+												inlineSize: `${logged ? '100%' : 'max-content'}`,
+												blockSize: 'min-content',
+												borderRadius: '0.5em',
+											}}
+										>
+											<AppRoutes />
+										</Content>
+									</Layout>
+								</Layout>
+							</>
+						) : (
+							<Layout
+								style={{
+									position: 'absolute',
+									inset: 0,
+									zIndex: 1,
+								}}
+							>
+								<Flex
+									style={{ inlineSize: '100%', blockSize: '100%' }}
+									justify='center'
+									align='center'
+								>
+									<Spin />
+								</Flex>
 							</Layout>
-						</Layout>
+						)}
 					</Layout>
 				</Router>
 			</Scrollbars>

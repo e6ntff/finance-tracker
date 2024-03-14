ba53edd3892c8b79fa8ad9c132488e1ce5e83f47
e6ntff <pashatar2@gmail.com>
2024-03-14 19:06:50 +0300

    refactored scrollbars

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import AppHeader from './components/AppHeader';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
@@ -58,16 +58,9 @@ const App: React.FC = observer(() => {
 
 	const { paddingLG, borderRadiusLG } = theme.useToken().token;
 
-	const ScrollbarsRef = useRef(null);
-
 	return (
 		<ConfigProvider theme={getConfig(isSmallScreen, themeAlgorithm)}>
-			<Scrollbars
-				autoHide
-				autoHideTimeout={1000}
-				autoHideDuration={200}
-				ref={ScrollbarsRef}
-			>
+			<Scrollbars autoHide>
 				<Router>
 					<Layout
 						style={{

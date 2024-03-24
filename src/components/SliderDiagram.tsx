a3d978795f28a9a502b645eb2c2a4395d1a41f90
e6ntff <pashatar2@gmail.com>
2024-03-24 17:24:31 +0300

    moved line diagram

---
+++ src/components/SliderDiagram.tsx
@@ -0,0 +1,70 @@
+import { observer } from 'mobx-react-lite';
+import React, { memo, useMemo } from 'react';
+import { Line } from 'react-chartjs-2';
+import { listStore } from 'utils/listStore';
+import { optionsStore } from 'utils/optionsStore';
+import { getValuesByMonth } from 'utils/transformData';
+
+const SliderDiagram: React.FC = observer(() => {
+	const { list } = listStore;
+	const { defaultRange } = optionsStore;
+
+	const valuesByMonth: number[] | { [key: string]: number } = useMemo(
+		() => getValuesByMonth(list, defaultRange),
+		[list, defaultRange]
+	);
+
+	const data = {
+		labels: new Array(valuesByMonth.length).fill(''),
+		datasets: [
+			{
+				data: valuesByMonth,
+			},
+		],
+	};
+
+	const options = {
+		plugins: {
+			legend: {
+				display: false,
+			},
+			tooltip: {
+				enabled: false,
+			},
+		},
+		scales: {
+			x: {
+				display: false,
+				grid: {
+					display: false,
+				},
+			},
+			y: {
+				display: false,
+				grid: {
+					display: false,
+				},
+			},
+		},
+		elements: {
+			point: {
+				radius: 0,
+			},
+			line: {
+				cubicInterpolationMode: 'monotone' as 'monotone',
+				borderColor: '#7775',
+			},
+		},
+		aspectRatio: 25,
+	};
+
+	return (
+		<Line
+			style={{ inlineSize: '100%' }}
+			data={data}
+			options={options}
+		/>
+	);
+});
+
+export default memo(SliderDiagram);

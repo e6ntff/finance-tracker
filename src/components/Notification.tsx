c5a511c8cd32885ab0d263d21c9cbfe0ee53015a
e6ntff <pashatar2@gmail.com>
2024-03-29 22:03:43 +0300

    changed notification

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,40 +1,24 @@
 import { message } from 'antd';
-import { ArgsProps } from 'antd/es/message';
 import { observer } from 'mobx-react-lite';
-import React, { useEffect, useMemo } from 'react';
-import languages from 'settings/languages';
-import { optionsStore } from 'utils/optionsStore';
+import React, { useEffect } from 'react';
+import constants from 'settings/constants';
 import { userStore } from 'utils/userStore';
 
 const Notification: React.FC = observer(() => {
 	const [messageApi, contextHolder] = message.useMessage();
 	const { notificationStatus } = userStore;
-	const { userOptions } = optionsStore;
-
-	const notifications: { [key: string]: ArgsProps } = useMemo(
-		() => ({
-			success: {
-				type: 'success',
-				content: <>{languages.success[userOptions.language]}</>,
-				duration: 1,
-			},
-			error: {
-				type: 'error',
-				content: <>{languages.error[userOptions.language]}</>,
-				duration: 1,
-			},
-			loading: {
-				type: 'loading',
-				content: <>{languages.loading[userOptions.language]}</>,
-				duration: 1,
-			},
-		}),
-		[userOptions.language]
-	);
 
 	useEffect(() => {
-		if (notificationStatus) {
-			messageApi.open(notifications[notificationStatus as string]);
+		if (
+			(notificationStatus.status === 'error' ||
+				notificationStatus.status === 'success') &&
+			notificationStatus.text
+		) {
+			messageApi.open({
+				type: notificationStatus.status,
+				content: <>{notificationStatus.text}</>,
+				duration: constants.errorDelay / 1000,
+			});
 		}
 		// eslint-disable-next-line
 	}, [notificationStatus]);

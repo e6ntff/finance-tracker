35ae1a415b0f5023a1ff34408da4f9330d517d31
e6ntff <pashatar2@gmail.com>
2024-04-12 12:33:22 +0300

    added rates ticker to header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -5,16 +5,28 @@ import { Flex } from 'antd';
 import { observer } from 'mobx-react-lite';
 import { userStore } from 'utils/userStore';
 import SyncIcon from './SyncIcon';
+import RatesTicker from './RatesTicker';
 
 const AppHeader: React.FC = observer(() => {
 	const { isSmallScreen, tourRefs } = userStore;
 
 	return (
 		<Flex
+			style={{ inlineSize: '100%' }}
 			justify='space-between'
 			align='center'
+			gap={isSmallScreen ? 8 : 16}
 		>
 			<Navigation />
+			{!isSmallScreen && (
+				<Flex
+					style={{ inlineSize: '25%' }}
+					vertical
+					align='stretch'
+				>
+					<RatesTicker />
+				</Flex>
+			)}
 			<Flex
 				ref={tourRefs[9]}
 				gap={isSmallScreen ? 8 : 16}
---
+++ src/components/RatesTicker.tsx
@@ -0,0 +1,51 @@
+import React, { CSSProperties, useMemo } from 'react';
+import { observer } from 'mobx-react-lite';
+import Marquee from 'react-fast-marquee';
+import { Alert, Flex, Typography } from 'antd';
+import { userStore } from 'utils/userStore';
+
+const RatesTicker: React.FC = observer(() => {
+	const { currencyRates } = userStore;
+
+	const { EURUSD, EURRUB, USDRUB } = useMemo(() => {
+		const { EUR, USD, RUB } = currencyRates;
+		return {
+			EURUSD: Math.round((USD / EUR) * 1000) / 1000,
+			EURRUB: Math.round((USD / EUR) * RUB * 1000) / 1000,
+			USDRUB: Math.round(RUB * 1000) / 1000,
+		};
+	}, [currencyRates]);
+
+	const text = useMemo(() => {
+		const textStyle: CSSProperties = {
+			color: '#fffa',
+			textAlign: 'center',
+			inlineSize: '15em',
+		};
+
+		return (
+			<Flex justify='space-around'>
+				<Typography.Text
+					style={textStyle}
+				>{`USD/RUB: ${USDRUB}`}</Typography.Text>
+				<Typography.Text
+					style={textStyle}
+				>{`EUR/RUB: ${EURRUB}`}</Typography.Text>
+				<Typography.Text
+					style={textStyle}
+				>{`EUR/USD: ${EURUSD}`}</Typography.Text>
+			</Flex>
+		);
+	}, [EURUSD, EURRUB, USDRUB]);
+
+	return (
+		<Alert
+			style={{ color: '#fffa', background: '#0000' }}
+			showIcon={false}
+			banner
+			message={<Marquee>{text}</Marquee>}
+		/>
+	);
+});
+
+export default RatesTicker;

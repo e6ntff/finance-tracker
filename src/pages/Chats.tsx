93bda98555320da94916b99a8e62733cca550092
e6ntff <pashatar2@gmail.com>
2024-04-15 15:49:47 +0300

    added adaptive to chats

--- src/pages/Chats.tsx
+++ src/pages/Chats.tsx
@@ -7,8 +7,11 @@ import CurrentChatHeader from 'components/CurrentChatHeader';
 import { observer } from 'mobx-react-lite';
 import React, { useRef, useState } from 'react';
 import Scrollbars, { positionValues } from 'react-custom-scrollbars';
+import { userStore } from 'utils/userStore';
 
 const Chats: React.FC = observer(() => {
+	const { isSmallScreen } = userStore;
+
 	const scrollbarsRef = useRef<Scrollbars | null>(null);
 
 	const [currentChatId, setCurrentChatId] = useState<string | null>(null);
@@ -22,49 +25,58 @@ const Chats: React.FC = observer(() => {
 
 	return (
 		<Flex style={{ blockSize: '75dvh' }}>
-			<Flex
-				vertical
-				style={{ inlineSize: '25%' }}
-			>
-				<ChatListHeader />
-				<Scrollbars autoHide>
-					<ChatList
-						selectedChatId={currentChatId}
-						selectChat={setCurrentChatId}
-					/>
-				</Scrollbars>
-			</Flex>
-			<Divider
-				type='vertical'
-				style={{ height: 'unset', margin: 0 }}
-			/>
-			<Flex
-				vertical
-				style={{ inlineSize: '75%' }}
-			>
-				<CurrentChatHeader chatId={currentChatId} />
-				<Divider />
-				<Scrollbars
-					onUpdate={onUpdate}
-					autoHide
-					ref={scrollbarsRef}
+			{((isSmallScreen && !currentChatId) || !isSmallScreen) && (
+				<Flex
+					vertical
+					style={{ inlineSize: isSmallScreen ? '100%' : '25%' }}
 				>
-					<CurrentChat
+					<ChatListHeader />
+					<Scrollbars autoHide>
+						<ChatList
+							selectedChatId={currentChatId}
+							selectChat={setCurrentChatId}
+						/>
+					</Scrollbars>
+				</Flex>
+			)}
+			{!isSmallScreen && (
+				<Divider
+					type='vertical'
+					style={{ height: 'unset', margin: 0 }}
+				/>
+			)}
+			{(!isSmallScreen || currentChatId) && (
+				<Flex
+					vertical
+					style={{ inlineSize: isSmallScreen ? '100%' : '75%' }}
+				>
+					<CurrentChatHeader
 						chatId={currentChatId}
-						scrollbarsRef={scrollbarsRef}
-						stuck={stuckToBottom}
-						setHasNewMessages={setHasNewMessages}
+						setCurrentChatId={setCurrentChatId}
 					/>
-				</Scrollbars>
-				{currentChatId && (
-					<ChatInput
-						chatId={currentChatId}
-						scrollbarsRef={scrollbarsRef}
-						stuck={stuckToBottom}
-						hasNewMessages={hasNewMessages}
-					/>
-				)}
-			</Flex>
+					<Divider />
+					<Scrollbars
+						onUpdate={onUpdate}
+						autoHide
+						ref={scrollbarsRef}
+					>
+						<CurrentChat
+							chatId={currentChatId}
+							scrollbarsRef={scrollbarsRef}
+							stuck={stuckToBottom}
+							setHasNewMessages={setHasNewMessages}
+						/>
+					</Scrollbars>
+					{currentChatId && (
+						<ChatInput
+							chatId={currentChatId}
+							scrollbarsRef={scrollbarsRef}
+							stuck={stuckToBottom}
+							hasNewMessages={hasNewMessages}
+						/>
+					)}
+				</Flex>
+			)}
 		</Flex>
 	);
 });

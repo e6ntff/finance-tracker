0f30aa6c24b19d2eb146109c6435b80f168bc2a4
e6ntff <pashatar2@gmail.com>
2024-04-21 17:27:59 +0300

    refactored welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -12,8 +12,6 @@ import Link from 'antd/es/typography/Link';
 import { Button, Flex, Form, Input, Tooltip, Typography } from 'antd';
 import { AuthUser } from 'settings/interfaces';
 import {
-	EyeInvisibleOutlined,
-	EyeOutlined,
 	LoginOutlined,
 	QuestionCircleOutlined,
 	WarningOutlined,
@@ -22,45 +20,38 @@ import { optionsStore } from 'utils/optionsStore';
 
 const auth = getAuth(app);
 
+const emptyFields = {
+	logIn: {
+		email: false,
+		password: false,
+	},
+	signIn: {
+		email: false,
+		password: false,
+		passwordAgain: false,
+	},
+};
+
+const emptyUser = {
+	logIn: {
+		email: '',
+		password: '',
+	},
+	signIn: {
+		email: '',
+		password: '',
+		passwordAgain: '',
+	},
+};
+
 const Welcome: React.FC = observer(() => {
 	const { setLogged, setIsNicknameModalOpened } = userStore;
 	const { userOptions } = optionsStore;
 
 	const { language } = userOptions;
 
-	const emptyFields = useMemo(
-		() => ({
-			logIn: {
-				email: false,
-				password: false,
-			},
-			signIn: {
-				email: false,
-				password: false,
-				passwordAgain: false,
-			},
-		}),
-		[]
-	);
-
-	const emptyUser = useMemo(
-		() => ({
-			logIn: {
-				email: '',
-				password: '',
-			},
-			signIn: {
-				email: '',
-				password: '',
-				passwordAgain: '',
-			},
-		}),
-		[]
-	);
-
 	const [mode, setMode] = useState<'logIn' | 'signIn'>('logIn');
 	const [incorrect, setIncorrect] = useState<boolean>(false);
-	const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
 	const [fields, setFields] = useState<AuthUser<boolean>>(emptyFields[mode]);
 	const [currentUser, setCurrentUser] = useState<AuthUser<string>>(
 		emptyUser[mode]
@@ -137,22 +128,9 @@ const Welcome: React.FC = observer(() => {
 			setFields(emptyFields[newMode]);
 			setCurrentUser(emptyUser[newMode]);
 			setIncorrect(false);
-			setIsPasswordVisible(false);
 			return newMode;
 		});
-	}, [
-		setMode,
-		setFields,
-		setCurrentUser,
-		setIncorrect,
-		setIsPasswordVisible,
-		emptyFields,
-		emptyUser,
-	]);
-
-	const handlePasswordVisibilityChange = () => {
-		setIsPasswordVisible((prev: boolean) => !prev);
-	};
+	}, [setMode, setFields, setCurrentUser, setIncorrect]);
 
 	useEffect(() => {
 		const signInInWithEnter = (key: any) => {
@@ -164,6 +142,79 @@ const Welcome: React.FC = observer(() => {
 		return () => window.removeEventListener('keyup', signInInWithEnter);
 	});
 
+	const emailInput = useMemo(
+		() => (
+			<Input
+				required
+				pattern='^[^\s@]+@[^\s@]+\.[^\s@]+$'
+				type='text'
+				value={currentUser.email}
+				onChange={handleEmailChange}
+			/>
+		),
+		[currentUser.email, handleEmailChange]
+	);
+
+	const requirementsTooltip = useMemo(
+		() => (
+			<Tooltip
+				title={languages.passwordRequirements[language]}
+				placement='right'
+			>
+				<QuestionCircleOutlined style={{ opacity: 0.5 }} />
+			</Tooltip>
+		),
+		[language]
+	);
+
+	const passwordInput = useCallback(
+		(repeat: boolean = false) => {
+			const value = repeat ? currentUser.passwordAgain : currentUser.password;
+
+			return (
+				<Input.Password
+					visibilityToggle={!repeat}
+					required
+					value={value}
+					onChange={(event) => handlePasswordChange(event, repeat)}
+				/>
+			);
+		},
+		[currentUser.password, currentUser.passwordAgain, handlePasswordChange]
+	);
+
+	const passwordsDoNotMatch = useMemo(
+		() => (
+			<Flex gap={4}>
+				<WarningOutlined />
+				<Typography.Text type='danger'>
+					{languages.passMatch[language]}
+				</Typography.Text>
+			</Flex>
+		),
+		[language]
+	);
+
+	const submitButton = useMemo(
+		() => (
+			<Tooltip
+				title={
+					mode === 'logIn'
+						? languages.logIn[language]
+						: languages.signIn[language]
+				}
+			>
+				<Button
+					disabled={!valid}
+					onClick={mode === 'logIn' ? logIn : signIn}
+				>
+					<LoginOutlined />
+				</Button>
+			</Tooltip>
+		),
+		[language, mode, logIn, signIn, valid]
+	);
+
 	return (
 		<Form
 			layout='vertical'
@@ -174,58 +225,20 @@ const Welcome: React.FC = observer(() => {
 				span: 26,
 			}}
 		>
-			<Form.Item label={languages.email[language]}>
-				<Input
-					required
-					pattern='^[^\s@]+@[^\s@]+\.[^\s@]+$'
-					type='text'
-					value={currentUser.email}
-					onChange={handleEmailChange}
-				/>
-			</Form.Item>
+			<Form.Item label={languages.email[language]}>{emailInput}</Form.Item>
 			<Form.Item
 				label={
 					<Flex gap={4}>
 						{languages.password[language]}
-						{mode === 'signIn' && (
-							<Tooltip
-								title={languages.passwordRequirements[language]}
-								placement='right'
-							>
-								<QuestionCircleOutlined style={{ opacity: 0.5 }} />
-							</Tooltip>
-						)}
+						{mode === 'signIn' && requirementsTooltip}
 					</Flex>
 				}
 			>
-				<Input
-					required
-					type={isPasswordVisible ? 'text' : 'password'}
-					value={currentUser.password}
-					onChange={handlePasswordChange}
-					suffix={
-						isPasswordVisible ? (
-							<EyeOutlined
-								style={{ opacity: 0.5 }}
-								onClick={handlePasswordVisibilityChange}
-							/>
-						) : (
-							<EyeInvisibleOutlined
-								style={{ opacity: 0.5 }}
-								onClick={handlePasswordVisibilityChange}
-							/>
-						)
-					}
-				/>
+				{passwordInput()}
 			</Form.Item>
 			{mode === 'signIn' && (
 				<Form.Item label={languages.repeatPassword[language]}>
-					<Input
-						required
-						type='password'
-						value={currentUser.passwordAgain}
-						onChange={(event) => handlePasswordChange(event, true)}
-					/>
+					{passwordInput(true)}
 				</Form.Item>
 			)}
 			<Flex
@@ -241,32 +254,13 @@ const Welcome: React.FC = observer(() => {
 					</Flex>
 				)}
 				{currentUser.password !== currentUser.passwordAgain &&
-					mode === 'signIn' && (
-						<Flex gap={4}>
-							<WarningOutlined />
-							<Typography.Text type='danger'>
-								{languages.passMatch[language]}
-							</Typography.Text>
-						</Flex>
-					)}
+					mode === 'signIn' &&
+					passwordsDoNotMatch}
 				<Flex
 					align='center'
 					gap={16}
 				>
-					<Tooltip
-						title={
-							mode === 'logIn'
-								? languages.logIn[language]
-								: languages.signIn[language]
-						}
-					>
-						<Button
-							disabled={!valid}
-							onClick={mode === 'logIn' ? logIn : signIn}
-						>
-							<LoginOutlined />
-						</Button>
-					</Tooltip>
+					{submitButton}
 					<Link onClick={changeMode}>
 						{mode === 'signIn'
 							? languages.already[language]
